35c16d1144fa40b08d6586d65f3ce183973a6ad1
guillaume.delattre.it@gmail.com <guillaume.delattre.it@gmail.com>
2026-03-02 18:21:18 +0100

    Devise reconfiguré avec les last et first name

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,28 @@
+class AccountsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to account_path, notice: "Compte mis à jour avec succès."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :last_name, :first_name, :email, :password, :password_confirmation)
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  protected
+
+  # Autoriser le champ personnalisé pour sign_up et account_update
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[last_name first_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[last_name first_name])
+  end
 end
