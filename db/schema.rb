bef50e64733fffa6d7f1731560161a385918c38e
guillaume.delattre.it@gmail.com <guillaume.delattre.it@gmail.com>
2026-03-03 12:23:08 +0100

    Ajout des ingrédients interdits dans users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_02_143905) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_03_112116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_02_143905) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "first_name", default: "", null: false
+    t.string "forbidden_ingredients"
     t.string "last_name", default: "", null: false
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
