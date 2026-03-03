bcf0c3cbc12a39ba5de73168eeb4023d5551ba1e
antoine rodrigues <antoine2000r@hotmail.fr>
2026-03-03 16:12:27 +0100

    liaison page 1 page 2

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,31 +1,25 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!
+
+  SYSTEM_PROMPT = "Tu es un chef cuisinier. Propose une recette avec ces ingrédients..."
+
   def index
     @recipes = Recipe.all
   end
 
-  def show
-    @recipe = Recipe.find(params[:id])
-
-  SYSTEM_PROMPT = "Tu es un chef cuisinier.
-    J'ai des ingrédients dans mon frigo mais je ne sais pas quoi préparer.
-    Réponds moi avec un nom de recette, ses ingrédients et les étapes de préparation"
-
   def create
-    @chat = current_user.chats.first_or_create!(title: "Nouvelle recherche")
-
-    @message = Message.new
-    @message.chat = @chat
-    @message.role = "user"
-    @message.content = params[:ingredients]
+    @chat = Chat.create!(user: current_user, title: "Recherche du #{Time.now.strftime('%d/%m')}")
+    @message = Message.new(chat: @chat, role: "user", content: params[:ingredients])
 
     if @message.save
-      llm_chat = RubyLLM.chat.with_instructions(SYSTEM_PROMPT)
-
-      response = llm_chat.ask(@message.content)
-
-      Message.create(content: response.content, role: "assistant", chat: @chat)
-
-      redirect_to swipe_recipes_path
+      begin
+        llm_chat = RubyLLM.chat.with_instructions(SYSTEM_PROMPT)
+        response = llm_chat.ask(@message.content)
+        Message.create!(content: response.content, role: "assistant", chat: @chat)
+      rescue RubyLLM::ConfigurationError
+        Message.create!(content: "IA en attente de configuration.", role: "assistant", chat: @chat)
+      end
+      redirect_to recipes_path
     else
       redirect_to root_path
     end
