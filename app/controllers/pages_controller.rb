c76d62ec7c96d9e01ed59a80b85b5e5d20dfcc3e
Kévin Delassat <kevin.delassat@gmail.com>
2026-03-03 21:24:27 +0100

    Modification routes + controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,4 @@
 class PagesController < ApplicationController
   def index
-    return unless user_signed_in?
-
-    chats = current_user.chats.order(created_at: :desc)
-    @chat = chats.first_or_create!(title: "Nouvelle recherche")
-    @messages = @chat.messages.order(created_at: :asc)
   end
 end
