aea492116cf1b8cb46072adf2801bdb932a2a60b
guillaume.delattre.it@gmail.com <guillaume.delattre.it@gmail.com>
2026-03-04 10:44:49 +0100

    AJout des colonnes desc et prepa_time dans recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_03_112116) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_04_094253) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -64,7 +64,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_03_112116) do
     t.datetime "created_at", null: false
     t.string "ingredient"
     t.text "preparation"
+    t.integer "preparation_time"
     t.integer "rating"
+    t.string "short_description"
     t.string "title"
     t.datetime "updated_at", null: false
   end
