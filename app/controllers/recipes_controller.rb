0224d42e6c8bb0a94ec61914deba9620ef2e9aba
Kévin Delassat <kevin.delassat@gmail.com>
2026-03-05 17:13:58 +0100

    badge

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -31,6 +31,7 @@ class RecipesController < ApplicationController
           is_healthy: data["is_healthy"],
           is_protein: data["is_protein"],
           is_gourmet: data["is_gourmet"],
+          # image_URL: fetch_image(data["title"])
         ).id
       end
 
@@ -72,7 +73,7 @@ class RecipesController < ApplicationController
   def system_prompt
     prompt = "Tu es un chef cuisinier. Je suis une personne qui n'a pas d'idée pour cuisiner un plat avec ce qu'il
     me reste dans mon frigo. Réponds UNIQUEMENT avec un tableau JSON de 2 recettes. Elles doivent avoir un nom, la liste
-    des ingrédients pour la préparer, la recette complète et détaillée avec le déroulé de plusieurs étapes, une durée de
+    des ingrédients pour la préparer, la recette complète et détaillée avec le déroulé de plusieurs étapes, une courte description de maximum 10 mots, une durée de
     préparation en minutes, et attribue une note aléatoire entre 3 et 5 arrondis à l'inférieur.
     à chaque recette, is_healthy et is_protein. Chaque élément de ta réponse doit impérativement être en français et
     le format de ta réponse doit être exactement celui-ci, sans texte autour, sans markdown.
